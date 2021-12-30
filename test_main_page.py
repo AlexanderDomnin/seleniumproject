0f7acc1769cb95c5694ae8d081be0a2de5272c3e
SonicX93 <alexanderdomnin1993@gmail.com>
2021-12-30 15:22:35 +0300

    -add allure

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
+import allure
 
 #Проверяем что переход по ссылке логина, ведет на корректную страницу с формами
 def test_guest_should_see_login_page(browser):
@@ -14,7 +15,7 @@ def test_guest_should_see_login_page(browser):
     login_page.should_be_login_page()
 
 #Ожидаем, что есть текст о том что корзина пуста
-@pytest.mark.stepik
+#@pytest.mark.stepik
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser,link)
@@ -24,14 +25,19 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.should_not_items()
     basket_page.should_not_items()
 
-@pytest.mark.login_guest
+#@pytest.mark.login_guest
+
 class TestLoginFromMainPage():
     # Проверка что ссылка логина есть на странице
+    @allure.feature('guest_can_go_to_login_page')
+    @allure.story('Проверка что ссылка логина есть на странице')
     def test_guest_can_go_to_login_page(self,browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        with allure.step("инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес"):
+            page = MainPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()  # открываем страницу
-        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        with allure.step("выполняем метод страницы — переходим на страницу логина"):
+            page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
     # Проверяем что можем перейти по ссылке логина
     def test_guest_should_see_login_link(self,browser):
